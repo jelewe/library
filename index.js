c0b676696dc125a002563851bb15399ebdf3b9c2
Jessie <hire.jesslw@gmail.com>
2022-10-09 08:49:39 -1000

    create function and styling to display Library array

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-let myLibrary = [];
+const shelf = document.querySelector('#content');
+
+let myLibrary = [
+   {
+    author: 'Vladimir Nabokov',
+    title: 'Pale Fire'
+   },
+   {
+    author: 'Kurt Vonnegut',
+    title: 'Slaughterhouse Five'
+   },
+   {
+    author: 'Cormac McCarthy',
+    title: 'The Road'
+   }
+
+];
 
 function Book() {
   // the constructor...
-}
+  Object.create 
+    this.author = author
+    this.title = title
+};
 
 function addBookToLibrary() {
   // do stuff here
-}
+    myLibrary.push(Book);
+    return myLibrary;
+};
+
+function displayLibrary() {
+  myLibrary.forEach(function (b) {
+    let div = document.createElement('div');
+    div.classList.add('book');
+    div.innerText = `${(b.title)}
+
+    ${(b.author)}`;
+    shelf.appendChild(div);
+  })
+};
+
+displayLibrary()
