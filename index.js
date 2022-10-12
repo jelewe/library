43e4b462779671f72acccc8b1c17c970c77b5627
Jessie <hire.jesslw@gmail.com>
2022-10-12 12:24:44 -1000

    add remove button and functionality

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const shelf = document.querySelector('#content');
 const submitBtn = document.querySelector('#submit');
 submitBtn.addEventListener('click', addBookToLibrary);
 
+const author = document.querySelector('#author');
+const title = document.querySelector('#title');
+const read = document.querySelector('#read');
+
 let myLibrary = [
    {
     author: 'Vladimir Nabokov',
@@ -22,31 +26,41 @@ let myLibrary = [
 
 ];
 
-function Book() {
+function Book(author, title, read) {
   // the constructor...
-    this.author = author;
-    this.title = title;
-    this.read = read;
-    return Book;
+   this.author= author.value;
+   this.title= title.value;
+   this.read= read.value;
 };
 
+function removeBook() {
+  let index = this.parentElement.getAttribute("data-index");
+  shelf.removeChild(this.parentElement);
+  myLibrary.splice(index, 1);
+  console.log(myLibrary);
+  return myLibrary;
+}
+
+
 function addBookToLibrary(event) {
-  // do stuff here
   event.preventDefault(); 
-  let newBook = {
-    author: document.getElementById('author').value,
-    title:document.getElementById('title').value,
-    read: document.getElementById('read').value
-  }
-   myLibrary.push(newBook);
-   console.log(myLibrary);
+  let newBook = new Book(author, title, read);
+  myLibrary.push(newBook);
    let div = document.createElement('div');
    div.classList.add('book');
+   div.setAttribute("data-index", myLibrary.indexOf(newBook));
    div.innerText = `${(newBook.title)}
 
    ${(newBook.author)}
    
-   ${(newBook.read)}`;
+   ${(newBook.read)}
+   
+   `;
+   let btn = document.createElement("button");
+   btn.classList.add('removeBtn');
+   btn.innerText="Remove";
+   btn.addEventListener('click', removeBook);
+   div.appendChild(btn);
    shelf.appendChild(div);
   document.querySelector('form').reset();
 };
@@ -55,13 +69,23 @@ function displayLibrary() {
   myLibrary.forEach(function (b) {
     let div = document.createElement('div');
     div.classList.add('book');
+    div.setAttribute("data-index", myLibrary.indexOf(b))
     div.innerText = `${(b.title)}
 
     ${(b.author)}
     
-    ${(b.read)}`;
+    ${(b.read)}
+    
+    `;
+    let btn = document.createElement("button");
+    btn.classList.add('removeBtn');
+    btn.innerText="Remove";
+    btn.addEventListener('click', removeBook);
+    div.appendChild(btn);
     shelf.appendChild(div);
+    return div;
   })
+  
 };
 
 displayLibrary()
